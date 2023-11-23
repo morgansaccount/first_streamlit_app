ed21541137ceac7be328500d21e749f55706cd30
Morgan Tucker <84941347+morgansaccount@users.noreply.github.com>
2023-11-23 14:15:22 -0600

    Update streamlit_app.py
    fruits to show component

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,9 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 # user interaction; pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display the table on the page
-streamlit.dataframe(my_fruit_list)
+
+# Display fruits_to_show on the page
+streamlit.dataframe(fruits_to_show)
