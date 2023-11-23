5ace48b1ef5384cb49d3761e579ab9900230fe2a
Morgan Tucker <84941347+morgansaccount@users.noreply.github.com>
2023-11-23 14:05:32 -0600

    Update streamlit_app.py
    fruit list df added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,14 @@ import streamlit
 
 streamlit.title('Breakfast Favorites')
 
-streamlit.header('Breakfast Menu')
-
 streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach, and Rocket Smoothie')
 streamlit.text('🐔 Hard-boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas as pd
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
